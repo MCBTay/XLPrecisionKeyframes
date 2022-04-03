415797191a006ae048463476f59ba33b5c8ab9af
mcbtay <mcbtay@gmail.com>
2022-04-03 12:45:29 -0400

    adding xml doc comments

--- XLPrecisionKeyframes/Patches/ReplayStatePatch.cs
+++ XLPrecisionKeyframes/Patches/ReplayStatePatch.cs
@@ -8,6 +8,9 @@ namespace XLPrecisionKeyframes.Patches
         [HarmonyPatch(typeof(ReplayState), nameof(ReplayState.OnEnter))]
         public static class OnEnterPatch
         {
+            /// <summary>
+            /// Patching into OnEnter to be able to create the user interface when entering ReplayState.
+            /// </summary>
             static void Postfix()
             {
                 //TODO: create keyframe menu
@@ -17,6 +20,9 @@ namespace XLPrecisionKeyframes.Patches
         [HarmonyPatch(typeof(ReplayState), nameof(ReplayState.OnExit))]
         public static class OnExitPatch
         {
+            /// <summary>
+            /// Patching into OnExit to be able to destroy the user interface when exiting ReplayState.
+            /// </summary>
             static void Postfix()
             {
                 //TODO: destroy keyframe menu

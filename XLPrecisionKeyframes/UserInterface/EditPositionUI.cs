1968d3392ca770e02bc60413060da91121c8c36f
mcbtay <mcbtay@gmail.com>
2022-04-03 17:51:48 -0400

    firstpass at edit position and edit rotation

---
+++ XLPrecisionKeyframes/UserInterface/EditPositionUI.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using XLPrecisionKeyframes.Keyframes;
+
+namespace XLPrecisionKeyframes.UserInterface
+{
+    public class EditPositionUI : EditBaseUI
+    {
+        public PositionInfo position;
+
+        private void OnGUI()
+        {
+            GUI.backgroundColor = Color.black;
+
+            var style = new GUIStyle(GUI.skin.window)
+            {
+                contentOffset = new Vector2(0, -20),
+                stretchHeight = false,
+                stretchWidth = false
+            };
+
+            GUILayout.Window(823, new Rect(40, 40, 200, 50), DrawWindow, "Edit Position", style);
+        }
+
+        private void DrawWindow(int windowID)
+        {
+            GUI.DragWindow(new Rect(0.0f, 0.0f, 10000f, 20f));
+            GUI.backgroundColor = Color.black;
+
+            GUILayout.BeginVertical();
+
+            CreatePositionControls();
+
+            GUILayout.BeginHorizontal();
+
+            if (GUILayout.Button("Save"))
+            {
+
+            }
+
+            if (GUILayout.Button("Cancel"))
+            {
+
+            }
+
+            GUILayout.EndHorizontal();
+
+            GUILayout.EndVertical();
+        }
+
+        private void CreatePositionControls()
+        {
+            GUILayout.BeginVertical();
+
+            position.x = CreateFloatField("X", position.x);
+            position.y = CreateFloatField("Y", position.y);
+            position.z = CreateFloatField("Z", position.z);
+
+            GUILayout.EndVertical();
+        }
+    }
+}
---
+++ XLPrecisionKeyframes/UserInterface/EditRotationUI.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using XLPrecisionKeyframes.Keyframes;
+
+namespace XLPrecisionKeyframes.UserInterface
+{
+    public class EditRotationUI : EditBaseUI
+    {
+        public RotationInfo rotation;
+
+        private void OnGUI()
+        {
+            GUI.backgroundColor = Color.black;
+
+            var style = new GUIStyle(GUI.skin.window)
+            {
+                contentOffset = new Vector2(0, -20),
+                stretchHeight = false,
+                stretchWidth = false
+            };
+
+            GUILayout.Window(823, new Rect(40, 40, 200, 50), DrawWindow, "Edit Rotation", style);
+        }
+
+        private void DrawWindow(int windowID)
+        {
+            GUI.DragWindow(new Rect(0.0f, 0.0f, 10000f, 20f));
+            GUI.backgroundColor = Color.black;
+
+            GUILayout.BeginVertical();
+
+            CreateRotationControls();
+
+            GUILayout.BeginHorizontal();
+
+            if (GUILayout.Button("Save"))
+            {
+
+            }
+
+            if (GUILayout.Button("Cancel"))
+            {
+
+            }
+
+            GUILayout.EndHorizontal();
+
+            GUILayout.EndVertical();
+        }
+
+        private void CreateRotationControls()
+        {
+            GUILayout.BeginVertical();
+
+            rotation.x = CreateFloatField("X", rotation.x);
+            rotation.y = CreateFloatField("Y", rotation.y);
+            rotation.z = CreateFloatField("Z", rotation.z);
+            rotation.w = CreateFloatField("W", rotation.w);
+
+            GUILayout.EndVertical();
+        }
+    }
+}

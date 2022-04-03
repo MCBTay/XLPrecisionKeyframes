0257b95d56056597258feb62800a4d0243abdbbb
mcbtay <mcbtay@gmail.com>
2022-04-03 12:28:18 -0400

    adding ReplayStatePatch and stubs for methods to create/destroy the keyframe menu

---
+++ XLPrecisionKeyframes/Patches/ReplayStatePatch.cs
@@ -0,0 +1,26 @@
+﻿using GameManagement;
+using HarmonyLib;
+
+namespace XLPrecisionKeyframes.Patches
+{
+    public static class ReplayStatePatch
+    {
+        [HarmonyPatch(typeof(ReplayState), nameof(ReplayState.OnEnter))]
+        public static class OnEnterPatch
+        {
+            static void Postfix()
+            {
+                //TODO: create keyframe menu
+            }
+        }
+
+        [HarmonyPatch(typeof(ReplayState), nameof(ReplayState.OnExit))]
+        public static class OnExitPatch
+        {
+            static void Postfix()
+            {
+                //TODO: destroy keyframe menu
+            }
+        }
+    }
+}

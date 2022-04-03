ff06ac0ff705572bfc9a106d25ea23ea45933a40
mcbtay <mcbtay@gmail.com>
2022-04-03 17:51:35 -0400

    creating EditBaseUI, which implements some common functionality between edit position/edit rotation

---
+++ XLPrecisionKeyframes/UserInterface/EditBaseUI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace XLPrecisionKeyframes.UserInterface
+{
+    public class EditBaseUI : MonoBehaviour
+    {
+        protected string CreateFloatField(string label, string value)
+        {
+            GUILayout.BeginHorizontal();
+
+            GUILayout.Label($"<b>{label}:</b>");
+
+            value = GUILayout.TextField(value, new GUIStyle(GUI.skin.textField)
+            {
+                alignment = TextAnchor.MiddleRight
+            });
+
+            GUILayout.EndHorizontal();
+
+            return value;
+        }
+    }
+}

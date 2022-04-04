108d456914af30c9b366c83be74ff65cb596e2d0
mcbtay <mcbtay@gmail.com>
2022-04-04 19:21:52 -0400

    adding FieldOfViewEditUI

---
+++ XLPrecisionKeyframes/UserInterface/FieldOfViewEditUI.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+namespace XLPrecisionKeyframes.UserInterface
+{
+    public class FieldOfViewEditUI : EditBaseUI
+    {
+        public float fov;
+        public float originalFov;
+
+        private string fovString;
+
+        public void SetFov(float fov)
+        {
+            this.fov = fov;
+            this.fovString = fov.ToString("F8");
+            this.originalFov = fov;
+        }
+
+        protected override void OnGUI()
+        {
+            StartingYPos = 300;
+            WindowLabel = "Edit FOV";
+
+            base.OnGUI();
+        }
+
+        protected override void DrawWindow(int windowID)
+        {
+            base.DrawWindow(windowID);
+
+            GUILayout.BeginVertical();
+            CreateFovControls();
+            CreateSaveAndCancelButtons();
+            GUILayout.EndVertical();
+        }
+
+        protected override void Save()
+        {
+            if (!float.TryParse(fovString, out var newFov)) return;
+
+            //ReplayEditorController.Instance.SetPlaybackTime(newTime);
+
+            base.Save();
+        }
+
+        private void CreateFovControls()
+        {
+            GUILayout.BeginVertical();
+
+            fovString = CreateFloatField("Field of View", fovString);
+
+            GUILayout.EndVertical();
+        }
+    }
+}

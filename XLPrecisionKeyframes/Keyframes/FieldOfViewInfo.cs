91a3f1ba509a9bfa5402391e4268627b77b47aea
mcbtay <mcbtay@gmail.com>
2022-04-04 19:13:15 -0400

    adding FOVInfo class

---
+++ XLPrecisionKeyframes/Keyframes/FieldOfViewInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace XLPrecisionKeyframes.Keyframes
+{
+    [Serializable]
+    public class FieldOfViewInfo
+    {
+        public string fov { get; set; }
+
+        public FieldOfViewInfo() : this(0)
+        {
+            
+        }
+
+        public FieldOfViewInfo(float fov)
+        {
+            this.fov = fov.ToString("F8");
+        }
+
+        public void Update(float fov)
+        {
+            this.fov = fov.ToString("F8");
+        }
+    }
+}

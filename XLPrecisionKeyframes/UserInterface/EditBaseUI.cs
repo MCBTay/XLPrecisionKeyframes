2979495bee5273b10b7a8afc7964f3e82ac719ec
mcbtay <mcbtay@gmail.com>
2022-04-03 21:49:54 -0400

    adding logic to calculate y pos

--- XLPrecisionKeyframes/UserInterface/EditBaseUI.cs
+++ XLPrecisionKeyframes/UserInterface/EditBaseUI.cs
@@ -1,9 +1,30 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 
 namespace XLPrecisionKeyframes.UserInterface
 {
     public class EditBaseUI : MonoBehaviour
     {
+        private bool HasKeyframes => UserInterface.keyFrames != null && UserInterface.keyFrames.Any();
+        private bool HasKeyframeName => !string.IsNullOrEmpty(UserInterface.currentKeyframeName);
+
+        protected float GetYPos(float originalYPos)
+        {
+            var yPos = originalYPos;
+
+            switch (HasKeyframes)
+            {
+                case true when HasKeyframeName:
+                    yPos += 45;
+                    break;
+                case true:
+                    yPos += 25;
+                    break;
+            }
+
+            return yPos;
+        }
+
         protected string CreateFloatField(string label, string value)
         {
             GUILayout.BeginHorizontal();
